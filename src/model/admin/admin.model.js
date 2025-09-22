43ff8cb81a7be8a5d0a3cb4bb1f9695c9bbfcb40
Muhammed Rishad <muhammed.rishad@koncpt.ai>
2025-09-22 17:15:07 +0530

    changes in db model

--- src/model/admin/admin.model.js
+++ src/model/admin/admin.model.js
@@ -2,10 +2,28 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../../config/database");
 
 const Admin = sequelize.define("Admin", {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  username: { type: DataTypes.STRING, allowNull: false, unique: true },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false }
-}, { timestamps: true });
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    defaultValue: sequelize.literal("nextval('custom_admin_id_seq')")
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  timestamps: true
+});
 
 module.exports = Admin;
